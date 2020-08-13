b34b73e93a4fa140d71287bd43787ef8f1da4b23
Dan <43259125+parise22@users.noreply.github.com>
2020-08-13 19:08:01 +1000

    Create CoinSpot.py

---
+++ CoinSpot.py
@@ -0,0 +1,44 @@
+import hmac
+from hashlib import sha512
+from time import time
+import json
+import requests
+
+class CoinSpot(object):
+    # CoinSpot -Read Only- API
+    API_ENDPOINT = "https://www.coinspot.com.au/api/ro/"
+
+    def __init__(self, apiKey, apiSecret):
+        self.key = apiKey
+        self.secret = apiSecret.encode()
+
+    # Generator
+    def _extract(self, data):
+        yield data
+
+    def _request(self, path, data=None):
+        if data is None:
+            data = {}
+
+        data["nonce"] = int(time() * 1000)
+        json_data = json.dumps(data, separators=(',', ':')).encode()
+
+        return requests.post(
+            self.API_ENDPOINT + path,
+            data=self._extract(json_data),
+            headers={
+                "Content-Type": "application/json",
+                "sign": hmac.new(self.secret, json_data, sha512).hexdigest(),
+                "key": self.key,
+            }
+        ).json()
+
+    def getLatest(self):
+        return requests.get("https://www.coinspot.com.au/pubapi/latest").json()
+
+    def myBalances(self):
+        return self._request("my/balances")
+
+    def myDeposits(self):
+        return self._request("my/deposits")
+
